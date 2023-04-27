33b08e7ecad278f5a077075b287e54ce310e4894
David Brown <davidbrown@corelogic.com>
2023-04-27 09:38:03 -0700

    feat: bootstrap sanity studio

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'cda06wlr',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {deskTool} from 'sanity/desk'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'sanity-poc',
+
+  projectId: 'cda06wlr',
+  dataset: 'production',
+
+  plugins: [
+    deskTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
